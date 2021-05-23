29f7539e0cabd3451f00f160b756a58d2eedac80
tele0724 <tele0724@gmail.com>
2021-05-23 11:54:50 +0900

    モデル、管理画面の追加

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from test_app.models import book_list
 
 # Register your models here.
+admin.site.register(book_list)
--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class book_list(models.Model):
+    book_name = models.CharField(verbose_name='本の名前', max_length=200)
+    author = models.CharField(verbose_name='作者', max_length=200)
+    publisher = models.CharField(verbose_name='出版社', max_length=200)
+    price = models.CharField(verbose_name='価格', max_length=200)
