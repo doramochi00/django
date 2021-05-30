2e1fb25380f567f96e764a91cdbec71b4282742c
tele0724 <tele0724@gmail.com>
2021-05-30 11:44:44 +0900

    Datatablesを使用するための処理追加

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
+from .views import DataTableView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('book_list/', views.view_book_list, name='book_list'),
+    path('book_list_datatables', TemplateView.as_view(template_name='test_app/book_list_datatables.html'), name='book_list_datatables'),
+    path('data/', DataTableView.as_view()),
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import book_list
+from django_datatables_view.base_datatable_view import BaseDatatableView
 
 def index(request):
     return render(request, 'test_app/index.html')
@@ -9,3 +10,13 @@ def view_book_list(request):
     data = book_list.objects.all()
     contents = {'data': data}
     return render(request, 'test_app/book_list.html', contents)
+
+class DataTableView(BaseDatatableView):
+    # モデルの指定
+    model = book_list
+    # 表示するフィールドの指定
+    columns = ['book_name', 'author', 'publisher', 'price']
+
+    # 検索方法の指定：部分一致
+    def get_filter_method(self):
+        return super().FILTER_ICONTAINS
